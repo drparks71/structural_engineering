be8a48aa6e7113eb6745ee3a034d2e28076b0f95
Dane.Parks <dane@daneparks.com>
2024-04-22 15:04:32 -0400

    Removed unused imports, updated license to use """ instead of '''

--- tests/test_beam_patterning.py
+++ tests/test_beam_patterning.py
@@ -1,5 +1,5 @@
 import unittest
-from Analysis.beam_patterning import load_pattern
+from Analysis.beam_patterning import load_pattern, pl
 
 
 class TestFunctions(unittest.TestCase):
@@ -7,7 +7,8 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(load_pattern(1), [[1]])
         self.assertEqual(load_pattern(2), [[1, 1], [0, 1], [1, 0]])
 
-
+    def test_pl(self):
+        self.assertEqual(pl())
 
 if __name__ == '__main__':
     unittest.main()
